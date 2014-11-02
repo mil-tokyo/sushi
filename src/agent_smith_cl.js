899dd2994626eaf93a0d32908bddde3ac823d097
miura <miura@mi.t.u-tokyo.ac.jp>
2014-11-02 13:46:32 +0900

    Fix memory leak

--- src/agent_smith_cl.js
+++ src/agent_smith_cl.js
@@ -31,10 +31,12 @@ if (nodejs) {
 	$CL.platform_info = $CL.platform.getInfo(WebCL.PLATFORM_NAME);
 	$CL.devices = $CL.platform.getDevices(WebCL.DEVICE_TYPE_DEFAULT);
 	$CL.device_info = $CL.devices[0].getInfo(WebCL.DEVICE_NAME);
+	// Create command queue
 	var context = WebCL.createContext({
 		deviceType : WebCL.DEVICE_TYPE_DEFAULT,
 		platform : $CL.platform
 	});
+	var queue = context.createCommandQueue($CL.devices[0], 0);
 	var createKernel = function(name, code) {
 		var program = context.createProgram(code);
 		program.build($CL.devices);
@@ -43,7 +45,7 @@ if (nodejs) {
 
 	var eachOperationGenerator = function(id, operator) {
 		// if the wises are same
-		var kernel = createKernel(
+		var kernel1 = createKernel(
 			"kernel_" + id + "_1",
 			"__kernel void kernel_" + id + "_1(__global float *a, __global float *b, uint iNumElements) " +
 			"{                                                                           " +
@@ -78,67 +80,42 @@ if (nodejs) {
 			if (mat1.rows !== mat2.rows || mat1.cols !== mat2.cols) {
 				throw new Error('shape does not match');
 			}
+			var kernel_to_use = null;
 			if (mat1.row_wise === mat2.row_wise) {
-				// Prepare buffer
-				var size = mat1.length * Float32Array.BYTES_PER_ELEMENT;
-				var aBuffer = context.createBuffer(WebCL.MEM_READ_WRITE, size);
-				var bBuffer = context.createBuffer(WebCL.MEM_READ_ONLY, size);
-				kernel.setArg(0, aBuffer);
-				kernel.setArg(1, bBuffer);
-				kernel.setArg(2, mat1.length, WebCL.type.UINT);
-	
-				// Create command queue
-				var queue = context.createCommandQueue($CL.devices[0], 0);
-	
-				// Execute the OpenCL kernel on the list
-				var localWS = [5]; // process one list at a time
-				var globalWS = [clu.roundUp(localWS, mat1.length)]; // process entire list
-	
-				// Do the work
-				queue.enqueueWriteBuffer(aBuffer, false, 0, size, mat1.data);
-				queue.enqueueWriteBuffer(bBuffer, false, 0, size, mat2.data);
-	
-				// Execute (enqueue) kernel
-				queue.enqueueNDRangeKernel(kernel, null, globalWS, localWS);
-	
-				// get results and block while getting them
-				queue.enqueueReadBuffer(aBuffer, true, 0, size, mat1.data);
-				
-				queue.release();
+				kernel_to_use = kernel1;
+			} else if (mat1.row_wise === true) {
+				kernel_to_use = kernel2;
 			} else {
-				var kernel_to_use = kernel2;
-				if (mat1.row_wise !== true) {
-					kernel_to_use = kernel3;
-				}
-				// Prepare buffer
-				var size = mat1.length * Float32Array.BYTES_PER_ELEMENT;
-				var aBuffer = context.createBuffer(WebCL.MEM_READ_WRITE, size);
-				var bBuffer = context.createBuffer(WebCL.MEM_READ_ONLY, size);
-				kernel_to_use.setArg(0, aBuffer);
-				kernel_to_use.setArg(1, bBuffer);
-				kernel_to_use.setArg(2, mat1.length, WebCL.type.UINT);
+				kernel_to_use = kernel3;
+			}
+			// Prepare buffer
+			var size = mat1.length * Float32Array.BYTES_PER_ELEMENT;
+			var aBuffer = context.createBuffer(WebCL.MEM_READ_WRITE, size);
+			var bBuffer = context.createBuffer(WebCL.MEM_READ_ONLY, size);
+			kernel_to_use.setArg(0, aBuffer);
+			kernel_to_use.setArg(1, bBuffer);
+			kernel_to_use.setArg(2, mat1.length, WebCL.type.UINT);
+			if (mat1.row_wise !== mat2.row_wise) {
 				kernel_to_use.setArg(3, mat1.rows, WebCL.type.UINT);
 				kernel_to_use.setArg(4, mat1.cols, WebCL.type.UINT);
-	
-				// Create command queue
-				var queue = context.createCommandQueue($CL.devices[0], 0);
-	
-				// Execute the OpenCL kernel on the list
-				var localWS = [5]; // process one list at a time
-				var globalWS = [clu.roundUp(localWS, mat1.length)]; // process entire list
-	
-				// Do the work
-				queue.enqueueWriteBuffer(aBuffer, false, 0, size, mat1.data);
-				queue.enqueueWriteBuffer(bBuffer, false, 0, size, mat2.data);
-	
-				// Execute (enqueue) kernel
-				queue.enqueueNDRangeKernel(kernel_to_use, null, globalWS, localWS);
-	
-				// get results and block while getting them
-				queue.enqueueReadBuffer(aBuffer, true, 0, size, mat1.data);
-				
-				queue.release();
 			}
+			
+			// Execute the OpenCL kernel on the list
+			var localWS = [5]; // process one list at a time
+			var globalWS = [clu.roundUp(localWS, mat1.length)]; // process entire list
+
+			// Do the work
+			queue.enqueueWriteBuffer(aBuffer, false, 0, size, mat1.data);
+			queue.enqueueWriteBuffer(bBuffer, false, 0, size, mat2.data);
+
+			// Execute (enqueue) kernel
+			queue.enqueueNDRangeKernel(kernel_to_use, null, globalWS, localWS);
+
+			// get results and block while getting them
+			queue.enqueueReadBuffer(aBuffer, true, 0, size, mat1.data);
+			
+			aBuffer.release();
+			bBuffer.release();
 		};
 	};
 	
@@ -235,9 +212,6 @@ if (nodejs) {
 			kernel_to_use.setArg(5, newM.cols, WebCL.type.UINT);
 			kernel_to_use.setArg(6, mat1.cols, WebCL.type.UINT);
 
-			// Create command queue
-			var queue = context.createCommandQueue($CL.devices[0], 0);
-
 			// Execute the OpenCL kernel on the list
 			var localWS = [5]; // process one list at a time
 			var globalWS = [clu.roundUp(localWS, mat1.length)]; // process entire list
@@ -252,7 +226,6 @@ if (nodejs) {
 			// get results and block while getting them
 			queue.enqueueReadBuffer(cBuffer, true, 0, newM.length * Float32Array.BYTES_PER_ELEMENT, newM.data);
 			
-			queue.release();
 			aBuffer.release();
 			bBuffer.release();
 			cBuffer.release();
