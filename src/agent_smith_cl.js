51507f5b6448d32decf3beb51262811e73070b11
miura <miura@mi.t.u-tokyo.ac.jp>
2015-01-27 12:11:32 +0900

    device selector

--- src/agent_smith_cl.js
+++ src/agent_smith_cl.js
@@ -167,19 +167,23 @@ if (typeof AgentSmith === 'undefined' || typeof AgentSmith.Matrix === 'undefined
 		}
 		
 		// device selector
-		var url_vars = function(){
-		    var vars = {}; 
-		    var param = location.search.substring(1).split('&');
-		    for(var i = 0; i < param.length; i++) {
-		        var keySearch = param[i].search(/=/);
-		        var key = '';
-		        if(keySearch != -1) key = param[i].slice(0, keySearch);
-		        var val = param[i].slice(param[i].indexOf('=', 0) + 1);
-		        if(key != '') vars[key] = decodeURI(val);
-		    } 
-		    return vars; 
-		}();
-		var device_index = url_vars.device_index ? Math.min(url_vars.device_index, $CL.devices.length - 1) : 0;
+		if (webcl) {
+			var device_index = 0;
+		} else {
+			var url_vars = function(){
+			    var vars = {}; 
+			    var param = location.search.substring(1).split('&');
+			    for(var i = 0; i < param.length; i++) {
+			        var keySearch = param[i].search(/=/);
+			        var key = '';
+			        if(keySearch != -1) key = param[i].slice(0, keySearch);
+			        var val = param[i].slice(param[i].indexOf('=', 0) + 1);
+			        if(key != '') vars[key] = decodeURI(val);
+			    } 
+			    return vars; 
+			}();
+			var device_index = url_vars.device_index ? Math.min(url_vars.device_index, $CL.devices.length - 1) : 0;
+		}
 		console.log('use device : ' + device_index);
 		
 		switch(env) {
